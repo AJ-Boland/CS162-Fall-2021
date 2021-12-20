db3585515a492f605c7217d1d444d89dbc28816c
AJ-Boland <andrew.boland.8965@mail.linnbenton.edu>
2021-12-19 16:26:58 -0800

    Test 2 of commiting changes! Got players loaded onto the board

--- Week 4+/classFile.py
+++ Week 4+/classFile.py
@@ -1,27 +1,33 @@
 
 
-
+#contains all non-sentient being on the game board
 class entity:
     #player id for if there is multiple players
     id = 0
 
-    def __init__(self, name, movement, team, starting_postion):
+    def __init__(self, name, movement, team, location):
         entity.id += 1;
         self.id = entity.id
         self.name = name
         self.movement = movement
+        self.team = team
         if(team == "enemy"):
             self.color = "red"
         if(team == "ally"):
             self.color = "blue"
         else:
             self.color = "black"
-        self.starting_postion = starting_postion
+        self.location = location
     
     def getID(self):
         #print(f"ID: {self.id}")
         return self.id
 
+    def getColor(self):
+        #print(f"ID: {self.id}")
+        return self.color
+    def printInfo(self):
+        print(f"Name: {self.name}, Movement: {self.movement}, Team: {self.team}, Current_pos: {self.location}")
 
 
 
@@ -30,22 +36,40 @@ class entity:
 
 
 
+#represents each tile on the board
 class tile:
-    id = 0
+    id = -1
     """
+    try:
+    
     if(occupied):
         occupied.color
     else:
     """
-    color = "gray"
-    def __init__(self,entity):
+   
+    def __init__(self):
         tile.id += 1
         self.id = tile.id
         self.coords = []
         self.entity = entity
+        self.color = tile.getEntityColor(self)
+    
+    def getEntityColor(self):
+        try:
+            if(self.entity.getColor()==None):
+                return "gray"
+            else:
+                return self.entity.getColor()
+
+        except:
+            print("Tile color recovery failure, defaulting")
+            return "gray"
+    
     def getID(self):
         #print(f"ID: {self.id}")
         return self.id
+    def printInfo(self):
+        print(f"ID: {self.id}, Color: {self.color}")
 
      
 
--- Week 4+/hello_tkinter_V3.py
+++ Week 4+/hello_tkinter_V3.py
@@ -1,36 +1,40 @@
 from tkinter import *
 from classFile import *
 
-
+#Global Variables
+board = []
+entity_list = []
 turn_value = 0
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Start of the tkinter loop
+"""
+root = Tk()
+root.geometry("800x600")
+
+#------------------Functions---------------#
 
 #passes (obj & int(0/1)) zero on first call to store player, then 1 on 2nd call to set player on new tile
 def player_select(obj,num):
     if(num == 0):
         pass;
 
-
-
-
-
-
-
-
-
-
-
-
-"""
-Below is the Tkinter visual making
-
-"""
-root = Tk()
-
-root.geometry("800x600")
-
+#Changes the color of the given tiles, obj is tile class
 def changeColor(obj):
     obj.color = 'blue'
     board_square = obj
@@ -50,6 +54,25 @@ def TileSelect(e):
                 print("hypothetical player select")
                 #player_select(obj, 0)
             break
+
+""" Decommissioned due to lack of use
+#User provides starting postion by x,y, we know tiles by id, so this returns the id of the addressed tile
+def convertCoords(x,y):
+    #check inputs
+    if ((x > 26 or y > 19) or (x < 0 or y < 0)):
+        print("Bad coordinate input")
+    else:
+        if(y != 1):
+            id = y*26 + x
+        else:
+            id = x
+        for obj in board:
+            if(obj.getID() == id):
+                return(obj)
+            else:
+                pass
+"""
+
         
         
 
@@ -62,19 +85,117 @@ x2 =35
 y1=5
 y2=35
 
-board = []
+
+#load entities
+file_object = open("scenario_0.txt")
+
+for line in file_object:   
+    #skips file comments :)
+    if (line[0] == "#"):
+        pass
+    else:
+        #input_check_Counter
+        ICCounter = 0
+        #Proper input check
+        for i in line:
+            if(i == ","):
+                ICCounter += 1
+        if (ICCounter != 4):
+            print("bad input check file, or check me cause that is possible 2")
+            break
+
+        else:
+            #Variables to be stored in entity object
+            name =""
+            movement = 0
+            team = ""
+            x = 0
+            y = 0
+            #Helper variables            
+            comma_counter = 0
+            digit_holder = 0
+            loop_counter = -1 #value starts at negative one so loop counter starts at zero
+            for i in line:
+                loop_counter += 1
+
+                #print(f" i:{i} counter: {comma_counter}")#Info check
+
+                #indicates what variable is being looked at
+                if(i == ","):
+                    comma_counter += 1
+                    pass
+
+                #Name == 0 Movement == 1 Team == 2 [x==3,y==4]
+                elif comma_counter == 0 :
+                    name = name + i
+
+                #movement
+                elif comma_counter == 1:
+                    if line[loop_counter+1] != ",":
+                        digit_holder += 1
+                        pass
+                    else:
+                        while digit_holder >= 0:
+                            movement = int(line[loop_counter - digit_holder]) * (10 ** digit_holder) + movement
+                            digit_holder -= 1
+                        digit_holder = 0
+
+                #team
+                elif comma_counter == 2:
+                    team = team + i
+
+                #x
+                elif comma_counter == 3:
+                    if i == "[":
+                        pass
+                    elif line[loop_counter+1] != ",":
+                        digit_holder += 1
+                        pass
+                    else:
+                        while digit_holder >= 0:
+                            x = int(line[loop_counter - digit_holder]) * (10 ** digit_holder) + x
+                            digit_holder -= 1
+                        digit_holder = 0
+
+                #y
+                elif comma_counter == 4:
+                    if i == "]":
+                        print("End")
+                        break
+                    elif line[loop_counter+1] != "]":
+                        digit_holder += 1
+                        pass
+                    else:
+                        while digit_holder >= 0:
+                            y = int(line[loop_counter - digit_holder]) * (10 ** digit_holder) + int(y)
+                            digit_holder -= 1
+                        digit_holder = 0
+            #post line loop            
+            Holder = entity(name,movement,team,[x,y])
+            entity_list.append(Holder)
 
 
 
-
+#load game board
 for j in range(0,19):
         
     for i in range(0,26):
         #
-        board_square = tile(0)
+        board_square = tile()
         board_square.coords = [x1,y1,x2,y2]
         board.append(board_square)
-        square = painting.create_rectangle(x1,y1,x2,y2,fill=tile.color) 
+
+        for creature in entity_list:            
+            if(i == (creature.location[0] - 1) and j == (creature.location[1] - 1)):
+                print(f"Tile infested: {board_square.getID()}")
+                print(f"Conditional: {(creature.location[0] - 1)}, {(creature.location[1] - 1)}")
+                board_square.entity = creature
+                board_square.color = board_square.getEntityColor()
+            else:
+                pass
+        
+        square = painting.create_rectangle(x1,y1,x2,y2,fill=board_square.color) 
+        board_square.printInfo()
        
         x1 = x1 + 30
         x2 = x2 + 30
@@ -84,15 +205,6 @@ for j in range(0,19):
     y2 = y2 + 30 
     x1 = 5
     x2 = 35
-
-#load entities
-file_object = open("scenario_0.txt")
-print(file_object.readline())
-
-
-
-
-
 
 
 root.bind("<Button 1>",TileSelect)
